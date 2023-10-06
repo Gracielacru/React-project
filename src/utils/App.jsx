cb46d5162997472b4e42c11b8d389ec939ec451f
Graciela de la Cruz Andrade <chelita600603@gmail.com>
2023-10-05 19:40:11 -0600

    https://github.com/Gracielacru/React-project.git

---
+++ src/utils/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+import Badge from "../badge/badge";
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello</h1> 
+     <Badge label= "Koko"/>
+    </div>
+  );
+}
+
+export default App;
